e7294b0c6d9fafd2578fa4d9fda1e2e2fc4d70f7
Sebastian Johannes Vogt <1620115@stud.hs-mannheim.de>
2017-04-21 09:47:49 +0200

    funktionen ohne funktion

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/Eis.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/Eis.java
@@ -2,4 +2,14 @@ package de.hsMannheim.tpe.gruppe21.ab02;
 
 public class Eis {
 
+	private String name;
+	private double preis;
+	private String behaeltnis;
+	private String art;
+	private String[] sorten;
+	private String[] extras;
+	
+	protected void vorbereiten(){};
+	protected void fuellen(){};
+	protected void dekorieren(){};
 }
--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/Eisdiele.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/Eisdiele.java
@@ -2,4 +2,21 @@ package de.hsMannheim.tpe.gruppe21.ab02;
 
 public class Eisdiele {
 
+	public void bestellen(String typ){
+		begruessen();
+		
+		if(eisZubereiten(typ)){
+			kassieren();
+		}else{
+			entschuldigen();
+		}
+		
+		verabschieden();
+	};
+	
+	protected boolean eisZubereiten(String typ){return false;}
+	protected void begruessen(){}
+	protected void kassieren(){}
+	protected void verabschieden(){}
+	protected void entschuldigen(){}
 }
