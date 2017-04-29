e720dc7fbe377a50dad8cf76fa7dfdb1ed60980b
Sebastian Johannes Vogt <1620115@stud.hs-mannheim.de>
2017-04-29 18:49:46 +0200

    ceasar crypter almost finished

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab022/Crypter.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab022/Crypter.java
@@ -1,11 +1,17 @@
 package de.hsMannheim.tpe.gruppe21.ab022;
 
 public interface Crypter {
-
 	
 	
+	/**
+	 * @param message gets encrypted
+	 * @return encrypted message
+	 */
 	public String encrypt(String message);
 	
-	
+	/**
+	 * @param cypherText gets decrypted
+	 * @return decrypted message
+	 */
 	public String decrypt(String cypherText);
 }
--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab022/CrypterCaesar.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab022/CrypterCaesar.java
@@ -1,13 +1,50 @@
 package de.hsMannheim.tpe.gruppe21.ab022;
 
 public class CrypterCaesar implements Crypter{
-
+	
+	private final int GROßZUKLEIN = 32;
+	private final int GROßA = 65;
+	private final int GROßZ = 90;
+	private final int KLEINA = 97;
+	private final int KLEINZ = 122;	
+	
 	@Override
 	public String encrypt(String message) {
-		// TODO Auto-generated method stub
-		return null;
+		int key = 5;
+		String newMessage = "";
+		for(int i = 0; i< message.length(); i++){
+			
+			if(!isLetter(message.charAt(i))){
+				//TODO: Throw new exception
+			}
+			
+			//Upper case
+			if(message.charAt(i) < KLEINA){
+				if(message.charAt(i)+ key > GROßZ){
+					newMessage += (char)(message.charAt(i)+key - 26);
+				}
+				else{
+					newMessage += (char)(message.charAt(i)+key);
+				}
+			}
+			//Lower case
+			else{
+				if(message.charAt(i)+ key > KLEINZ){
+					newMessage += (char)(message.charAt(i)+key - 26);
+				}
+				else{
+					newMessage += (char)(message.charAt(i)+key);
+				}
+			}
+		}
+		
+		return newMessage;
 	}
-
+	
+	public boolean isLetter(char toTest){
+		return ((GROßA <= toTest &&  toTest <= GROßZ) || (KLEINA <= toTest &&  toTest <= KLEINZ));
+	}
+	
 	@Override
 	public String decrypt(String cypherText) {
 		// TODO Auto-generated method stub
