bb7e3235a3745e6c92c22471fbdfb42ab297a2fe
Sebastian Johannes Vogt <1620115@stud.hs-mannheim.de>
2017-05-02 13:53:19 +0200

    k

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/comparableSort/MyInt.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/comparableSort/MyInt.java
@@ -1,6 +1,27 @@
 package de.hsMannheim.tpe.gruppe21.ab02.comparableSort;
 
 public class MyInt implements Comparable {
-
+	
+	private int value;
+	
+	public MyInt(int value){
+		this.value = value;
+	}
+	
+	public int getValue(){
+		return value;
+	}
+	
+	public int compareTo(Object other){
+		if(((MyInt)(other)).getValue() < this.getValue()){
+			return 1;
+		}
+		else if(((MyInt)(other)).getValue() < this.getValue()){
+			return 0;
+		}
+		else{
+			return -1;
+		}
+	}
 	
 }
