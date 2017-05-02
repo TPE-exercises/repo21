e1cf9d7a8319261c3b55fd85abc368bc45b06ceb
Sebastian Johannes Vogt <1620115@stud.hs-mannheim.de>
2017-05-02 12:38:07 +0200

    fertiger insert

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/InsertionSort.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/InsertionSort.java
@@ -1,5 +1,21 @@
 package de.hsMannheim.tpe.gruppe21.ab02;
 
 public class InsertionSort {
-
+	
+	public static void insertionSort(Comparable[] compareArray){
+		for(int i = 1; i < compareArray.length; i++){
+			for(int j = i; j > 1; j++){
+				if(compareArray[j].compareTo(compareArray[j-1]) == -1){
+					swap(compareArray, j, j+1);
+				}
+			}
+		}
+	}
+	
+	private static void swap(Comparable[] compareArray, int objectOne, int objectTwo){
+		Comparable swapper = compareArray[objectOne];
+		compareArray[objectOne] = compareArray[objectTwo];
+		compareArray[objectTwo] = swapper;
+	}
+	
 }
