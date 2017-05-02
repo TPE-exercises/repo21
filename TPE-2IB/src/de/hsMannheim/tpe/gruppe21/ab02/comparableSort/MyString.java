0382deb7fa13a3ffc5acc1e5efc2c51bc1a07e7a
Sebastian Johannes Vogt <1620115@stud.hs-mannheim.de>
2017-05-02 13:45:27 +0200

    jk

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/comparableSort/MyString.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab02/comparableSort/MyString.java
@@ -1,5 +1,14 @@
 package de.hsMannheim.tpe.gruppe21.ab02.comparableSort;
 
 public class MyString implements Comparable{
+	private String value;
+	
+	public MyString(String value){
+		this.value = value;
+	}
+	
+	public int compareTo(MyString other){
+		return -1;
+	}
 	
 }
