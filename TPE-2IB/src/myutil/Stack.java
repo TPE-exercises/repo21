634ae3e6555db7607781674c75b41c07c93ab137
Pascal-PC <Pascal-PC@DESKTOP-EV7THV0.fritz.box>
2017-05-04 20:51:36 +0200

    Jetzt sogar mit kommentaren

--- TPE-2IB/src/myutil/Stack.java
+++ TPE-2IB/src/myutil/Stack.java
@@ -3,21 +3,46 @@ package myutil;
 public interface Stack {
 
 	
+	
+	/**
+	 * 
+	 * @param element to put on stack
+	 * @return Stack with element
+	 */
 	public Stack push(int element);
 	
 	
+	/**
+	 * Removes element on top of stack
+	 * @return removed element
+	 */
 	public int pop();
 	
 	
+	/**
+	 * Get element on top of stack
+	 * @return element on top of the stack
+	 */
 	public int top();
 	
 	
+	/**
+	 * Creates an empty stack
+	 * @return empty stack
+	 */
 	public Stack empty();
 	
 	
+	/**
+	 * Checks if stack is empty
+	 * @return true if stack is empty, false if not
+	 */
 	public boolean isEmpty();
 	
-	
+	/**
+	 * 
+	 * @return size of stack
+	 */
 	public int size();
 	
 }
