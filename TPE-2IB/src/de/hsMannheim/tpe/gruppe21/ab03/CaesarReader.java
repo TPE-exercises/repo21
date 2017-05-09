9afc809a1c373e665fe75c9f7fbe47aa5564d3da
Sebastian Johannes Vogt <1620115@stud.hs-mannheim.de>
2017-05-09 12:14:28 +0200

    reader auch fertig

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab03/CaesarReader.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab03/CaesarReader.java
@@ -1,10 +1,12 @@
 package de.hsMannheim.tpe.gruppe21.ab03;
 
-import java.io.FilterReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 
 public class CaesarReader extends FilterReader {
+	
+	public static final char[] caesarAlph = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+			'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Ä', 'Ö', 'Ü', 'ä', 'ö', 'ü' };
 	
 	private int key;
 
@@ -14,19 +16,39 @@ public class CaesarReader extends FilterReader {
 	}
 
 	@Override
-	public int read() throws IOException {
-		super.read();
-		// TODO Auto-generated method stub
-		return 0;
+	public int read() throws IOException {		
+		return caesarDecrypt((char)super.read());
 	}
 
 	@Override
 	public int read(char[] cbuf, int off, int len) throws IOException {
-		super.read(cbuf, off, len);
-		// TODO Auto-generated method stub
-		return 0;
+		int ret = super.read(cbuf, off, len);
+		for(int i = 0; i < cbuf.length; i++){
+			cbuf[i] = caesarDecrypt(cbuf[i]);
+		}
+		return ret;
 	}
 
+	private char caesarDecrypt(char toDecrypt){
+		int position = caesarAlph.length;
+		for(int i = 0; i < caesarAlph.length; i++){
+			if(caesarAlph[i] == toDecrypt){
+				position = i;
+			}
+		}
+		position -= key;
+		//if there is an underflow
+		while(position < 0){
+			position += caesarAlph.length;
+		}
+		if(position < caesarAlph.length){
+			return caesarAlph[position];
+		}
+		//else:= toCrypt is special char
+		else{
+			return toDecrypt;
+		}
+	}
 	
 	
 }
