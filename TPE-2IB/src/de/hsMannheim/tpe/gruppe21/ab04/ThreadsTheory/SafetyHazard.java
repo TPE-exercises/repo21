f1d669636bfb8af0b2900597860d8092408ebb59
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2017-05-30 18:09:06 +0200

    kein plan

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab04/ThreadsTheory/SafetyHazard.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab04/ThreadsTheory/SafetyHazard.java
@@ -1,18 +1,43 @@
 package de.hsMannheim.tpe.gruppe21.ab04.ThreadsTheory;
 
-public class SafetyHazard {
+public class SafetyHazard{
 
+	public static int kontostand = 1000;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Kontostand: " + kontostand);
+		HazardThread abb500 = new HazardThread(-500);
+		
 	}
-
+	
 	/*
 	1: Safety Hazards 
-	Wenn mehrere Threads gleichzeitig auf Resoursen zugreifen wollen müssen diese synchornisiert werden. Ansonsten kann es ein, dass zum Beispiel
-	ein Thread eine Variable liest, diese Verändert aber bevor er diese wieder in den Speicher schreiben kann ein anderer Thread die Varaible liest
-	und mit dem alten / noch nicht akutalisierten Wert weiterrechnet. Dadurch verhält sich das Programm in Anwesenheit von mehreren Threads nicht 
-	mehr wie erwartet / korrekt.
+	Wenn mehrere Threads gleichzeitig auf Resoursen zugreifen wollen müssen diese synchornisiert werden. 
+	Ansonsten kann es ein, dass zum Beispiel ein Thread eine Variable liest, 
+	diese Verändert aber bevor er diese wieder in den Speicher schreiben kann ein anderer Thread die Varaible 
+	liest und mit dem alten noch nicht akutalisierten Wert weiterrechnet. 
+	Dadurch verhält sich das Programm in Anwesenheit von mehreren Threads nicht mehr wie erwartet korrekt.
 	*/
 	
+}	
+class HazardThread extends Thread{
+	public int buchung;
+	
+	HazardThread(int buchung){
+		this.buchung = buchung;
+	}
+	
+	@Override
+	public void run(){
+		int speicher = kontostand;
+		speicher += this.buchung;
+		try {
+			this.wait(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		kontostand = speicher;
+	}
 }
+
+ 
