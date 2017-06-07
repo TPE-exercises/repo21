8bc48df23c7f7fb5adffe5efee5cba7a2acd5a85
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2017-06-07 11:25:00 +0200

    kaldf

---
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab04/Quicksort/SortAlgorithm.java
@@ -0,0 +1,5 @@
+package de.hsMannheim.tpe.gruppe21.ab04.Quicksort;
+
+public interface SortAlgorithm {
+
+}
--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab04/ThreadsTheory/LivenessHazard.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab04/ThreadsTheory/LivenessHazard.java
@@ -1,25 +1,20 @@
 package de.hsMannheim.tpe.gruppe21.ab04.ThreadsTheory;
 
 public class LivenessHazard extends Thread{
-
-	public static int zahl = 10;
-	
 	
 	public static void main(String[] args) {
-		System.out.println("Zahl: " + zahl);
-		LivenessHazard ersterThread;
-		LivenessHazard zweiterThread;
-		ersterThread  = new LivenessHazard(zweiterThread);
-		zweiterThread = new LivenessHazard(ersterThread);
+		LivenessHazard ersterThread = new LivenessHazard();
+		LivenessHazard zweiterThread = new LivenessHazard();
 		ersterThread.start();
 		zweiterThread.start();
-		System.out.println("Zahl: " + zahl);
 	}
 	
 	/*
 	2. Liveness	Hazards	
 	
 	*/
+	
+	public boolean isFinished = false;
 	
 	public void run(){
 		
