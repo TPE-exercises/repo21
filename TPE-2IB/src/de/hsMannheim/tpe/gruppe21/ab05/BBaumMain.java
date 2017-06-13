eec24ad9ac394a758d3281467bf1d23d74210fc5
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2017-06-13 17:32:41 +0200

    delete funktioniert

--- TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab05/BBaumMain.java
+++ TPE-2IB/src/de/hsMannheim/tpe/gruppe21/ab05/BBaumMain.java
@@ -3,19 +3,13 @@ package de.hsMannheim.tpe.gruppe21.ab05;
 public class BBaumMain {
 
 	public static void main(String[] args) {
-		BTree test = new BTree(2);
-		test.insert(5);
-		test.insert(26);
-		test.insert(23);
-		test.insert(4);
-		test.insert(2);
-		test.insert(13);
+		BTree test = new BTree(5,"rndmNmbrs.txt");
+		test.printInorder();
+		
+		test.delete(50);
 		
 		test.printInorder();
 		
-		test.delete(5);
-		
-		test.printInorder();
 	}
 
 }
